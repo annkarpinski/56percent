88a8fe3f22a03631d3636c2ebeec5ea71d2def14
Omar Sulehria <osulehria@rallydev.com>
2013-06-02 01:14:52 -0600

    back to basics, a text/reply scenario

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ FOLLOWUP_FEELIGNS = %w(Carefree Peaceful Relieved Mellow Relaxed)
 omars_phone = '+17208787118'
 anns_phone = '+13038271604'
 
-txt_msg_counter = 0
+txt_msg_counter = 1
 
 def validate_reply(reply, text_message_number)
   return true if INITIAL_FEELINGS.include? reply && text_message_number == 1
@@ -49,6 +49,12 @@ def create_response(body)
     twiml.text
 end
 
+sms_client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+sms_client.account.sms.messages.create(
+  :from => TWILIO_PHONE,
+  :to => omars_phone,
+  :body => create_first_text_message
+)
 
 get '/' do
     'Welcome to the prototype version of 56percent, where we empower women to become leaders.'
@@ -64,22 +70,12 @@ get '/send-message' do
   body = params[:Body] || "No text"
   response = ""
 
-  if (txt_msg_counter == 0)
-    sms_client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-    sms_client.account.sms.messages.create(
-      :from => TWILIO_PHONE,
-      :to => omars_phone,
-      :body => create_first_text_message
-    )
-    txt_msg_counter += 1
-  end 
-
   # if (validate_reply(body, txt_msg_counter))
-    case txt_msg_counter
-    when 1
+    # case txt_msg_counter
+    # when 1
       response = create_response(create_second_text_message(body))
-      txt_msg_counter += 1
-    end
+      # txt_msg_counter += 1
+    # end
   # else
   #   create_response("We have no idea how to answer that, please try again.")
   # end
