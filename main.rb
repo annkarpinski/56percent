e500aedf363aa9294583581ce3957a7a22f6bfdb
Omar Sulehria <osulehria@rallydev.com>
2013-06-02 00:41:41 -0600

    giving it a go with context-aware sms responses

--- main.rb
+++ main.rb
@@ -3,10 +3,21 @@ require 'twilio-ruby'
 require 'sinatra'
 require_relative 'auth_info'
 
+NAME = 'Ann'
+INITIAL_FEELINGS = %w(Passionate Energized Connected Hopeful Aligned)
+FOLLOWUP_FEELIGNS = %w(Carefree Peaceful Relieved Mellow Relaxed)
+
 omars_phone = '+17208787118'
 anns_phone = '+13038271604'
 
 txt_msg_counter = 1
+
+def validate_reply(reply, text_message_number)
+  return true if INITIAL_FEELINGS.include? reply && text_message_number == 1
+  return true if FOLLOWUP_FEELIGNS.include? reply && text_message_number == 2
+
+  false
+end
 
 # Had to alter first text due to message going over 160 character limit
 def create_first_text_message
@@ -19,58 +30,55 @@ Aligned
 )
 end
 
-# First question reponse handling here...
-def handle_first_text_response
-
+# Second text question which is under 160 characters
+def create_second_text_message(first_response)
+%(When I feel #{first_response}, I will also feel:
+Carefree
+Peaceful 
+Relieved
+Mellow
+Relaxed)
 end
 
-# Second question...
-def create_second_text_message
-end
-
-# Response to second question here...
-def handle_second_text_response
-
+# Generic response helper method
+def create_response(body)
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.sms body
+    end
 end
 
 @sms_client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
-# @sms_client.account.sms.messages.create(
-#   :from => TWILIO_PHONE,
-#   :to => omars_phone,
-#   :body => create_first_text_message
-# )
+
 
 get '/' do
-    'Currently running version ' + Twilio::VERSION + \
-        ' of the twilio-ruby library.'
+    'Welcome to the prototype version of 56percent, where we empower women to become leaders.'
 end
 
 get '/send-message' do
-  sender = params[:From]
-  text = params[:Body]
-
   friends = {
     "+13038271604" => "Ann",
     "+17208787118" => "Omar"
   }
 
-  body = text || "No text"
+  sender = params[:From]
+  body = params[:Body] || "No text"
 
-  personal_name = friends[sender] || "Flower Power"
-  twiml = Twilio::TwiML::Response.new do |r|
-    # case txt_msg_counter
-    # when 1
-    #   handle_first_text_response
-    #   txt_msg_counter += 1
-    # when 2
-    #   handle_second_text_response
-    #   txt_msg_counter = 0
-    # else
-    #   puts "Should not get here..."
-    # end
-
-    r.Sms "Hey you said this: #{body}, thanks for replying!"
+  if (validate_reply(body, txt_msg_counter))
+    case txt_msg_counter
+    when 0
+      @sms_client.account.sms.messages.create(
+        :from => TWILIO_PHONE,
+        :to => omars_phone,
+        :body => create_first_text_message
+      )
+      txt_msg_counter += 1
+    when 1
+      create_response(create_second_text_message(body))
+      txt_msg_counter += 1
+    else
+      create_response("We have no idea how to answer that, please try again.")
+    end 
   end
 
   twiml.text
