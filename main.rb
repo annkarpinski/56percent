34f9f3da1a52a2c7826e601407ebe4d412bd3a0b
Omar Sulehria <osulehria@rallydev.com>
2013-06-01 22:51:16 -0600

    changed return value of send-message endpoint and added text message counter (experimental)

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require_relative 'auth_info'
 omars_phone = '+17208787118'
 anns_phone = '+13038271604'
 
+txt_msg_counter = 1
+
 # Had to alter first text due to message going over 160 character limit
 def create_first_text_message
 %(Today, I want to feel:
@@ -15,6 +17,18 @@ Connected
 Hopeful
 Aligned
 )
+end
+
+# First question reponse handling here...
+def handle_first_text_response
+end
+
+# Second question...
+def create_second_text_message
+end
+
+# Response to second question here...
+def handle_second_text_response
 end
 
 @sms_client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
@@ -40,16 +54,18 @@ get '/send-message' do
 
   personal_name = friends[sender] || "Flower Power"
   twiml = Twilio::TwiML::Response.new do |r|
+    case txt_msg_counter
+    when 1
+      handle_first_text_response
+      txt_msg_counter += 1
+    when 2
+      handle_second_text_response
+      txt_msg_counter = 0
+    else
+      puts "Should not get here..."
+    end
+
     r.Sms "Hey, #{personal_name}, thanks for replying!"
   end
-  twiml.text
+  sender
 end
-
-
-# First question reponse handling here...
-
-
-# Second question...
-
-
-# Response to second question here...
